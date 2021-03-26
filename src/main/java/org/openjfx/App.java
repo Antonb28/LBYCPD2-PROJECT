3a7b660f2f5c6ceed55a96971f68d7372ee57421
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2021-03-26 14:44:55 +0800

    Log-In

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("LogInScreen"));
         stage.setScene(scene);
         stage.show();
     }
