3814e5dfd8c7810836eeb9543fa618c05a6cca87
Jeremiah-Jensen <jeremiah_jensen@dlsu.edu.ph>
2021-03-27 13:00:00 +0800

    Hello

---
+++ src/main/java/org/openjfx/Admin.java
@@ -0,0 +1,11 @@
+package org.openjfx;
+
+import javafx.event.ActionEvent;
+
+public class Admin {
+    public void ViewPatientInfo(ActionEvent event) {
+    }
+
+    public void ViewDoctorInfo(ActionEvent event) {
+    }
+}
--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -15,9 +15,7 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-
     private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("LogInScreen"));
