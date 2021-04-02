4f5dbe2bee88f485730da4ad6be6d84ded23d82a
Jeremiah-Jensen <jeremiah_jensen@dlsu.edu.ph>
2021-04-02 15:10:00 +0800

    Imported firebase again.

--- src/main/java/org/openjfx/Register.java
+++ src/main/java/org/openjfx/Register.java
@@ -1,9 +1,16 @@
 package org.openjfx;
 
+import javafx.fxml.FXML;
+
 import java.io.IOException;
 
 public class Register {
+    @FXML
     public void LogIn() throws IOException {
         App.setRoot("LogInScreen");
     }
+
+
 }
+
+
